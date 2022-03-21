1d6fccb7f842532c31c3d143a560080d72e67c61
daeng1502 <daengfebrino01@gmail.com>
2022-03-22 00:28:32 +0700

    Check

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
 app.get('/', function (req, res) {
-  res.send('Selamat Datang di Projek LOKI')
+  res.send('Selamat Datang di Projek LOKI Kelompok 8')
 })
 app.listen(3000)
