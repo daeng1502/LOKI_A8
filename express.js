3a7f6ee90ae867b8330c07d35d0223b05dda16b7
daeng1502 <daengfebrino01@gmail.com>
2022-03-24 16:52:28 +0700

    router 1

--- express.js
+++ express.js
@@ -3,4 +3,10 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Selamat Datang di Projek LOKI Kelompok 8')
 })
+
+app.get('/new', (req,res)=> {
+  res.send("User New Form")
+})
+
+
 app.listen(3000)
