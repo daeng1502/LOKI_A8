e5346ef98c0901453668d08b670875508dfd7c0f
RRaidan <edward5raidan54@gmail.com>
2022-03-24 17:26:15 +0700

    tet

--- express.js
+++ express.js
@@ -3,4 +3,18 @@ var app = express()
 app.get('/', function (req, res) {
   res.send('Selamat Datang di Projek LOKI Kelompok 8')
 })
+
+app.get('/', function(req, res){
+  res.send('Silakan login disini');
+});
+
+app.get('/', function(req, res){
+  res.send('Terimakasih dan sampai jumpa');
+});
+
+app.get('/', function(req, res){
+  res.send('silakan tuliskan nama user');
+});
+
+
 app.listen(3000)
