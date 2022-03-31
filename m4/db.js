5d0b5d7bf817f720df03d553a78e0813c1046a65
daeng1502 <daengfebrino01@gmail.com>
2022-03-31 21:34:54 +0700

    siap

---
+++ m4/db.js
@@ -0,0 +1,65 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize("mysql://root@localhost/loki-a8")
+
+sequelize.define('courses' ,
+{
+    id:
+    {
+        type : DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey : id
+    },
+
+    curriculum_id:
+    {
+        type : DataTypes.STRING,
+        allowNull: false,
+        foreignKey : curriculum_id
+    },
+
+    code:
+    {
+        type : DataTypes.STRING,
+        allowNull: false 
+    }, 
+
+    name:
+    {
+        type : DataTypes.STRING,
+        allowNull: false 
+    },
+
+    alias_name:
+    {
+        type : DataTypes.STRING,
+        allowNull: false 
+    },
+
+    credit:
+    {
+        type : DataTypes.INTEGER,
+        allowNull : false
+    },
+
+    semester:
+    {
+        type : DataTypes.INTEGER,
+        allowNull : false
+    },
+
+    description:
+    {
+        type : DataTypes.STRING,
+        allowNull : false
+    },
+
+    created_at:
+    {
+        type : DataTypes.DATE,
+    },
+
+    updated_at:
+    {
+        type : DataTypes.DATE,
+    }
+});
