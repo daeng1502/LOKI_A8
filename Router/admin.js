002686f5f98c1aa35f05521cdb0ff87907099f3d
TaufikHidayat1001 <91662214+TaufikHidayat1001@users.noreply.github.com>
2022-03-31 21:08:51 +0700

    Update admin.js

--- Router/admin.js
+++ Router/admin.js
@@ -6,7 +6,7 @@ router.get('/login', function(req, res) {
 })
 
 router.get('/logout', function(req, res) {
-    res.send("Halaman Logout Admin")
+    res.send("Halaman Logout Adminn")
 })
 
 router.get('/menentukan_dosen', function(req, res) {
