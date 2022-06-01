b8301e90e026267e8f0513b8ce3a8de860610a76
vallenadithya <2011522017_vallen@student.unand.ac.id>
2022-06-01 14:10:42 +0700

    rebuld man

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 5050
+const port = 8000
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+dotenv.config();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.json())
+app.set('view engine', 'ejs');
+app.use( express.static( "views" ) );
+
 
 app.get('/', function (req, res) {
   res.send('Selamat Datang di Projek LOKI Kelompok 8')
@@ -16,4 +27,4 @@ app.get("/print", function(req, res) {
     res.send("Cetak RPS")
 })
 
-app.listen(5050)
+app.listen(8000)
