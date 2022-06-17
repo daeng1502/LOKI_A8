fd90abe022b2b116c985e9604251d0cb458ddb2e
vallenadithya <2011522017_vallen@student.unand.ac.id>
2022-06-17 15:51:15 +0700

    Progress frontend

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 42069
+const port = 6969
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
@@ -10,29 +10,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.json())
-app.set('view engine', 'ejs');
-app.use( express.static( "views" ) );
+app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.use( express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('index')
 })
 
+app.get('/login', function (req, res) {
+  res.render('login-page')
+})
+
 app.get('/mahasiswa', function (req, res) {
-  res.render('Mahasiswa/dashboard')
+  res.render('mhs-dashboard')
 })
 
-app.get('/mahasiswa/detail-rps', function (req, res) {
-  res.render('Mahasiswa/rps')
+app.get('/mahasiswa-detailrps', function (req, res) {
+  res.render('mhs-rps')
+})
+
+app.get('/admin/home', function (req, res) {
+  res.render('admin-home')
 })
 
 app.get('/admin/home', function (req, res) {
   res.render('/Admin/home')
 })
 
-app.get('/admin/home', function (req, res) {
-  res.render('/Admin/home')
-})
-
-app.listen(42069)
+app.listen(port);
